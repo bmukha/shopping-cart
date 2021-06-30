f00bf26c865d17815903c373589ad61796d6ecdf
Bohdan <by.muha@gmail.com>
2021-06-30 19:21:23 +0300

    grid works-2

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,11 @@ import { AppBar, Toolbar } from "@material-ui/core";
 import React, { useContext } from "react";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import Badge from "@material-ui/core/Badge";
+import Typography from "@material-ui/core/Typography";
+import { Link } from 'react-router-dom';
+import Home from "./Home";
+import Shop from "./Shop";
+
 // import { Context } from "./Context";
 
 export default function Header() {

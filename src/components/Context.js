ed366c818ed11914fb4efdc6238500400159a950
Bohdan <by.muha@gmail.com>
2021-07-01 19:42:52 +0300

    details page bug fixed

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { products } from "./products";
 
 const Context = React.createContext();
 
 function ContextProvider({ children }) {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(products);
   const [cartItems, setCartItems] = useState([]);
-
-  useEffect(() => {
-    setItems(products);
-  }, []);
 
   return <Context.Provider value={{items, setCartItems, cartItems}}>{children}</Context.Provider>;
 }
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,18 @@
 import React, { useContext } from "react";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { Context } from "./Context";
 
 export default function Details() {
-  const { items } = useContext(Context);
+  const contextValue = useContext(Context);
   const { itemId } = useParams();
-  const currItem = items.find((item) => item.id == itemId);
+  console.log(contextValue);
+  console.log(itemId);
+  const currItem = contextValue.items.find((item) => item.id == itemId);
+  console.log(currItem);
   return (
     <div>
       <h1>{currItem.title}</h1>
+      <img src={currItem.image} alt=""></img>
     </div>
   );
 }
